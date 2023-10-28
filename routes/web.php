026fcad6d38b7056ba20c957cc9735f4a615c4a3
Lagudas Joyce <143520079+joycekrystal@users.noreply.github.com>
2023-10-28 22:32:43 +0800

    init

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::prefix('/products')->group(function(){
+    Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/store', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/update', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('/destroy', [ProductController::class, 'destroy'])->name('products.destroy');
+    Route::get('/view/{id}', [ProductController::class, 'show'])->name('products.show');
 });
